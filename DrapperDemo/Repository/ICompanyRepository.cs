2e444bcbc3b0c84040b0515285949fd8f0e3fbf5
djabirpc <djabir.pc@outlook.fr>
2021-08-28 22:16:06 +0100

    Add project files.

---
+++ DrapperDemo/Repository/ICompanyRepository.cs
@@ -0,0 +1,17 @@
+﻿using DrapperDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DrapperDemo.Repository
+{
+    public interface ICompanyRepository
+    {
+        Company Find(int id);
+        List<Company> GetAll();
+        Company Add(Company company);
+        Company Update(Company company);
+        void Remove(int id);
+    }
+}
